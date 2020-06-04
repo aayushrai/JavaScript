1b08818b43c2e24b25d81c8418bc98c9a2ed2175
aayushrai <www.aayushrai@gmail.com>
2020-06-04 15:37:08 +0530

    data type and type convertion

---
+++ dataType.js
@@ -0,0 +1,36 @@
+//primitive type
+
+//string
+const name = "aayush rai";
+console.log(typeof name)
+// Number 
+const age = 45;
+console.log(typeof age)
+//
+const is = true;
+console.log(typeof is)
+//Null
+const car = null;  
+console.log(typeof car) 
+//undefined
+let test;
+console.log(typeof test)
+// symbol
+const sym = Symbol();
+console.log(typeof sym)
+
+//reference types
+
+//Array
+const hobbies = ["games","music"];
+console.log(typeof hobbies);
+
+// Object literal
+const address = {city:"indore",state:"Mp"};
+console.log(typeof address);
+
+//time
+const today = new Date();
+console.log(today);
+console.log(typeof today);
+
---
+++ typeConvertion.js
@@ -0,0 +1,68 @@
+let val;
+
+// Number to string
+val = 55;
+
+//Output
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+val = String(val);
+
+//Output
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+//Bool to string
+
+iscycle = true;
+
+//Output
+console.log(iscycle);
+console.log(typeof iscycle);
+console.log(iscycle.length);
+
+iscycle = String(iscycle)
+//Output
+console.log(iscycle);
+console.log(typeof iscycle);
+console.log(iscycle.length);
+
+//Array to string
+
+val = String([1,2,3,4])
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+//date to string
+
+val = String(new Date())
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+// toString methode
+val = (5).toString();
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+// toString methode
+val = (true).toString();
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+// String to number
+val = Number("55");
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
