f877085b9c218561f027170ffd9cf85f36f6ff72
aayushrai <www.aayushrai@gmail.com>
2020-06-05 17:29:04 +0530

    Examining dom

--- DOM/examining_document.js
+++ DOM/examining_document.js
@@ -9,4 +9,19 @@ val = document.doctype;
 val = document.domain;
 val = document.URL;
 val = document.contentType;
+val = document.characterSet;
+
+val = document.forms;
+val = document.forms[0];
+
+val = document.links;
+val = document.links[0].className;
+val = document.scripts
+val = document.scripts[2].getAttribute('src')
+let scriptsCollection = document.scripts;
+scriptsCollection = Array.from(scriptsCollection);
+
+scriptsCollection.forEach(function(script){
+ console.log(script);
+});
 console.log(val);
