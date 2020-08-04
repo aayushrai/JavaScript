e7eb723421eee051cdee6efd16175321b2a58f46
aayushrai <www.aayushrai@gmail.com>
2020-08-04 16:26:58 +0530

    load file

---
+++ Ajax/readText.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click", loadData);
+
+
+function loadData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "data.txt", true);
+    xhr.onload = function() {
+        if (this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    xhr.send()
+}
